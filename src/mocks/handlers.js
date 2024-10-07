5076a3f82f119cfc89af061c1d61dacfed7c2900
Alsona Natsi <alsona.naci1188@gmail.com>
2024-10-07 02:07:51 +0000

    Writing some tests to check our NavBar functionality.

---
+++ src/mocks/handlers.js
@@ -0,0 +1,24 @@
+import { rest } from "msw";
+
+const baseURL = "https://petspace-api-195e436e05ae.herokuapp.com/";
+
+export const handlers = [
+  rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
+    return res(
+      ctx.json({
+        pk: 5,
+        username: "luna",
+        email: "",
+        first_name: "",
+        last_name: "",
+        profile_id: 2,
+        profile_image:
+          "https://res.cloudinary.com/dbu8nqoae/image/upload/v1/media/images/pexels-yuliya-shabliy-126574-388_wltdjl",
+
+      })
+    );
+  }),
+  rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
+    return res(ctx.status(200));
+  }),
+];
